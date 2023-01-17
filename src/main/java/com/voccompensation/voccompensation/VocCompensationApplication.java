47fec5bcf83cb3d629c8b4167300af2e06c63920
JeongJuRyu <totw2018@naver.com>
2023-01-17 19:03:24 +0900

    First Commit

---
+++ src/main/java/com/voccompensation/voccompensation/VocCompensationApplication.java
@@ -0,0 +1,13 @@
+package com.voccompensation.voccompensation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocCompensationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocCompensationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/voccompensation/voccompensation/VocCompensationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voccompensation.voccompensation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocCompensationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
